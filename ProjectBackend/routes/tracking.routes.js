550f017a6478302ec0cf60e45816b4a4256f7251
Rui Marques <ruimarques0826@gmail.com>
2024-03-13 14:13:58 +0000

    routes

--- ProjectBackend/routes/tracking.routes.js
+++ ProjectBackend/routes/tracking.routes.js
@@ -34,7 +34,7 @@ router.put("/trackings/:trackingId", isAuthenticated, (req, res) => {
     
     const { trackingId } = req.params;
     Tracking.findById(trackingId).then((response) => {
-        if (response.data.createdBy === _id) {
+        if (response.createdBy.toString() === _id) {
             const { bodyWeight, steps, duration, kcals } = req.body;
                 Tracking.findByIdAndUpdate(trackingId, {bodyWeight, steps, duration, kcals })
                 .then(() => {
@@ -48,27 +48,18 @@ router.put("/trackings/:trackingId", isAuthenticated, (req, res) => {
             res.status(401).json({message: "You are not the owner of the tracker!"})
         }
     })
-    
-});
+    })
 
-router.delete("/trackings/:trackingId", isAuthenticated, (req, res) => {
-    const { _id } = req.payload;
-
+router.delete("/trackings/:trackingId", (req, res) => {
     const { trackingId } = req.params;
-    Tracking.findById(trackingId).then((response) => {
-        if (response.data.createdBy === _id) {
-            Tracking.findByIdAndDelete(trackingId)
-            .then(() => {
-            res.json({message: "Tracker was deleted!"})
-        })
-            .catch(() => {
-            res.json({message: "Tracker was not deleted!"})
-        });
-    }
-    else {
-        res.status(401).json({message: "You are not the owner of the tracker to delete it!"})
-    }
-})
+
+    Tracking.findByIdAndDelete(trackingId)
+    .then(() => {
+        res.json({message: "Tracking was deleted!"})
+    })
+    .catch(() => {
+        res.json({message: "Tracking was not deleted!"})
+    });
 });
 
 module.exports = router;
